2fce69771cdc13c37bdf562176828f9977f56b36
Camilla D'Andrea <camilladandrea@live.it>
2020-05-23 12:17:45 +0200

    controller2

--- MioEsame/src/main/java/com/exam/Controller/Controller.java
+++ MioEsame/src/main/java/com/exam/Controller/Controller.java
@@ -2,6 +2,8 @@ package com.exam.Controller;
 
 import java.util.*;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.exam.Database.DatabaseMetadata;
@@ -22,7 +24,7 @@ public class Controller {
 	 * @return la collezione di metadati
 	 */
 	
-@GetMapping("/getMetadati")
+	@RequestMapping(value = "metadata", method=RequestMethod.GET)
 	public ArrayList<Metadati> getAll()
 	{
 		return DatabaseMetadata.getAll();
