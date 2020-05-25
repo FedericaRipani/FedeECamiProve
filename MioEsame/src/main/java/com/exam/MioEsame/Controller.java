95d91f94d19bf3905f0d8ea7bbcda453651175fd
Camilla D'Andrea <camilladandrea@live.it>
2020-05-25 11:21:25 +0200

    HttpRequest aggiunto

--- MioEsame/src/main/java/com/exam/MioEsame/Controller.java
+++ MioEsame/src/main/java/com/exam/MioEsame/Controller.java
@@ -2,6 +2,8 @@ package com.exam.MioEsame;
 
 import java.util.*;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,14 +23,16 @@ public class Controller {
 	
 	/**
 	 * Gestisce la chiamata che fa visualizzare i metadati
+	 * con un codice HTTP 200
 	 * @return la collezione di metadati
 	 */
 	
-	@GetMapping(" /getMeta")
-	public ArrayList<Metadati> getAll()
-	{
-		return DatabaseMetadata.getAll();
+	@GetMapping("/getMeta")
+	public ResponseEntity<ArrayList<Metadati>> meta() {
+		return new ResponseEntity<ArrayList<Metadati>>(DatabaseMetadata.getAll(), HttpStatus.OK);
+	
 	}
 	
+
 
 }
