46225daa3f9ac826e6ed81d790b87105c281cf8b
FedericaRipani <federchicca99@hotmail.com>
2020-05-28 23:34:41 +0200

    >Aggiunta classe astratta e eliminazione FilterService

--- MioEsame/src/main/java/com/exam/Filter/UpperBound.java
+++ MioEsame/src/main/java/com/exam/Filter/UpperBound.java
@@ -4,18 +4,26 @@ import java.util.ArrayList;
 import com.exam.Database.*;
 import com.exam.model.Tweet;
 
-public class UpperBound(int param){
+public class UpperBound(){
 	
 	private int parametroFiltro;
 	
-	public UpperBound(int par) {
+	public int parametro (int param) {
+		int Param = param;
+		return Param;
+	}
+	
+
+	
+	public UpperBound(int Param) {
+		int par;
 		this.parametroFiltro=par;
 	}
 	
 	public static ArrayList<Tweet> data = DatabaseTweet.getAll();
 	public ArrayList<Tweet> filteredArray = new ArrayList<Tweet>();
 	
-	public int length = data.size();
+	/*public*/ int length = data.length();
 	
 	for(int j=0; j<length; j++)
 		if(data.listed_count[j]>parametroFiltro)
---
+++ MioEsame/src/main/java/com/exam/Interface/AbstractFilter.java
@@ -0,0 +1,39 @@
+package com.exam.Interface;
+
+import java.util.ArrayList;
+
+import org.json.JSONException;
+
+import com.exam.model.Tweet;
+
+/**
+ * 
+ * Classe astratta creata per includere i filtri logici e condizionali da
+ * implementare. I filtri dovranno di conseguenza implementare i metodi approved
+ * e applica.
+ *
+ */
+
+public abstract class AbstractFilter {
+	
+	
+		private String fields;
+
+		public AbstractFilter(String fields) {
+			setFields(fields);
+		}
+
+		public String getFields() {
+			return fields;
+		}
+
+		public void setFields(String fields) {
+			this.fields = fields;
+		}
+
+		public abstract boolean approved(Tweet elem, boolean equal) throws JSONException;
+
+		public abstract ArrayList<Tweet> apply(ArrayList<Tweet> list, boolean equal) throws JSONException;
+	
+
+}
